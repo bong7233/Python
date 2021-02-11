2fcc46965c8da5b51ff3bae1015f57af5b60d5d6
Lee sangbong <77820303+bong7233@users.noreply.github.com>
2021-02-11 18:26:44 +0900

    Add files via upload

---
+++ 13_input.py
@@ -0,0 +1,10 @@
+# Chapter05_02
+# Input
+# Input의 기본타입은 str형임을 주의############
+# 그러므로 input앞에 str이아니라면 자료형 선정해야함
+
+# 연습
+name = input("Enter your name")
+grade = input("Enter your grade")
+
+print(name, grade, company)
