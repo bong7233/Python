107e72811cabbe6979f12b45f393bd06228a4cde
bong7233 <77820303+bong7233@users.noreply.github.com>
2021-03-27 10:59:41 +0900

    Add 파이썬의 자료구조형

--- Sequence.py
+++ Sequence.py
@@ -1,1 +1,6 @@
-# Sequence
+# 시퀀스형
+# 파이썬에서는 데이터를 두종류로나눔
+# 컨테이너 자료형(Container : 서로다른 자료형을 담는것[list,tuple,collections.deque])
+# 플랫(Flat : 한개의 자료형만 담는것[str, bytes, bytearray, array.array, memoryview])
+# 가변형(list, bytearray, array.array, memoryview, deque)
+# 불변형(tuple, str, bytes)
